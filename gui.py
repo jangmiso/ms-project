cacbdf29c83ef6aaa66bba9a1a1c1b5e9420ae4d
CHAEWON25 <116560377+CHAEWON25@users.noreply.github.com>
2022-12-08 23:55:52 +0900

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,8 +1,18 @@
 import sys
 from PyQt5.uic import loadUi
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QLabel
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QDate, QTime
+
+from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
+from mplwidget import MplWidget
+import numpy as np
+import random
+
+import sqlite3
+
+
 
 
 
@@ -10,7 +20,7 @@ class WelcomeScreen(QDialog):
     def __init__(self):
         super(WelcomeScreen, self).__init__()
         bglabel = QLabel(self)
-        pixmap = QPixmap('/home/pi/prj/gui/디자인/welcome_sc2.jpg')
+        pixmap = QPixmap('/home/pi/prj/gui/디자인/welcome_sc_or.jpg')
         bglabel.setPixmap(pixmap)
         loadUi("welcomescreen.ui", self)
         self.start.clicked.connect(self.gotologin)
@@ -25,7 +35,7 @@ class LoginScreen(QDialog):
     def __init__(self):
         super(LoginScreen, self).__init__()
         bglabel2 = QLabel(self)
-        pixmap = QPixmap('/home/pi/prj/gui/디자인/login_sc.jpg')
+        pixmap = QPixmap('/home/pi/prj/gui/디자인/login_sc_or.jpg')
         bglabel2.setPixmap(pixmap)
         loadUi("login.ui", self)
         self.passwordfield.setEchoMode(QtWidgets.QLineEdit.Password)
@@ -40,8 +50,113 @@ class LoginScreen(QDialog):
             self.error.setText("아이디를 입력하세요!")
         elif len(password)==0:
             self.error.setText("비밀번호를 입력하세요!")
+        else:   
+            conn = sqlite3.connect("jibjung.db")
+            cur = conn.cursor()
+
+            query = 'SELECT passwd FROM member WHERE id='+user+""
+            
+            cur.execute(query)
+            
+            result = cur.fetchone()[0]
+
+            if str(result) == str(password):
+                main = MainScreen(password)
+                widget.addWidget(main)
+                widget.setCurrentIndex(widget.currentIndex()+1)
+                
 
 
+class MainScreen(QDialog):
+    def __init__(self,password):
+        super(MainScreen, self).__init__()
+        loadUi("main3.ui", self)
+        
+ 
+        
+        label_user = QLabel(self)
+        label_user.setGeometry(QtCore.QRect(60, 20, 111, 111))
+        label_user.resize(110, 110)
+        pixmap = QPixmap('/home/pi/prj/gui/디자인/user (2).png')
+        label_user.setPixmap(pixmap)
+        label_user.setScaledContents(True)
+        
+        label_point = QLabel(self)
+        label_point.setGeometry(QtCore.QRect(508, 47, 45, 45))
+        label_point.resize(45, 45)
+        pixmap = QPixmap('/home/pi/prj/gui/디자인/coin (1).png')
+        label_point.setPixmap(pixmap)
+        label_point.setScaledContents(True)
+        
+        
+        self.pushButton_generate_random_signal.clicked.connect(self.update_graph)
+
+        #self.addToolBar(NavigationToolbar(self.MplWidget.canvas, self))
+
+        
+        self.bt_home.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page))
+        self.bt_mypage.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_1))
+        self.bt_class.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_2))
+        self.bt_bestworst.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_3))
+        
+        
+        self.label_gold = QtWidgets.QLabel(self.page_3)
+        self.label_gold.setGeometry(QtCore.QRect(40, 180, 70, 70))
+        self.label_gold.setStyleSheet("background-color: rgba(255, 255, 255, 0);")
+        self.label_gold.setText("")
+        self.label_gold.setPixmap(QtGui.QPixmap("/home/pi/prj/gui/디자인/gold-medal.png"))
+        self.label_gold.setScaledContents(True)
+        
+        self.label_silver = QtWidgets.QLabel(self.page_3)
+        self.label_silver.setGeometry(QtCore.QRect(40, 280, 70, 70))
+        self.label_silver.setStyleSheet("background-color: rgba(255, 255, 255, 0);")
+        self.label_silver.setText("")
+        self.label_silver.setPixmap(QtGui.QPixmap("/home/pi/prj/gui/디자인/silver-medal.png"))
+        self.label_silver.setScaledContents(True)
+        self.label_silver.setObjectName("label_silver")
+        self.label_bronze = QtWidgets.QLabel(self.page_3)
+        self.label_bronze.setGeometry(QtCore.QRect(40, 380, 70, 70))
+        self.label_bronze.setStyleSheet("background-color: rgba(255, 255, 255, 0);")
+        self.label_bronze.setText("")
+        self.label_bronze.setPixmap(QtGui.QPixmap("/home/pi/prj/gui/디자인/bronze.png"))
+        self.label_bronze.setScaledContents(True)
+        self.label_bronze.setObjectName("label_bronze")
+        self.label_gold_2 = QtWidgets.QLabel(self.page_3)
+        self.label_gold_2.setGeometry(QtCore.QRect(530, 180, 70, 70))
+        self.label_gold_2.setStyleSheet("background-color: rgba(255, 255, 255, 0);")
+        self.label_gold_2.setText("")
+        self.label_gold_2.setPixmap(QtGui.QPixmap("/home/pi/prj/gui/디자인/gold-medal.png"))
+        self.label_gold_2.setScaledContents(True)
+        self.label_gold_2.setObjectName("label_gold_2")
+        self.label_silver_2 = QtWidgets.QLabel(self.page_3)
+        self.label_silver_2.setGeometry(QtCore.QRect(530, 280, 70, 70))
+        self.label_silver_2.setStyleSheet("background-color: rgba(255, 255, 255, 0);")
+        self.label_silver_2.setText("")
+        self.label_silver_2.setPixmap(QtGui.QPixmap("/home/pi/prj/gui/디자인/silver-medal.png"))
+        self.label_silver_2.setScaledContents(True)
+        self.label_silver_2.setObjectName("label_silver_2")
+        self.label_bronze_2 = QtWidgets.QLabel(self.page_3)
+        self.label_bronze_2.setGeometry(QtCore.QRect(530, 380, 70, 70))
+        self.label_bronze_2.setStyleSheet("background-color: rgba(255, 255, 255, 0);")
+        self.label_bronze_2.setText("")
+        self.label_bronze_2.setPixmap(QtGui.QPixmap(":/icon/디자인/bronze.png"))
+        self.label_bronze_2.setScaledContents(True)
+        self.label_bronze_2.setObjectName("label_bronze_2")
+    
+    def update_graph(self):
+
+        f = random.randint(1, 20)
+        f2=random.randint(10, 30)
+        
+
+        self.MplWidget.canvas.axes.clear()
+        self.MplWidget.canvas.axes.plot([0,1,2,3,4],[5*f,f2,2*f,3*f2,4*f])
+        self.MplWidget.canvas.axes.legend(('point', 'pointt'),loc='upper right')
+        #self.MplWidget.canvas.axes.set_title('Cosinus - Sinus Signal')
+        self.MplWidget.canvas.draw()
+        
+        
+        
 
 
 
